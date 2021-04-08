01e9c58eafd43af047691b2c3c7d4566810be079
bozh-anka <81514272+bozh-anka@users.noreply.github.com>
2021-04-08 22:25:53 +0300

    style

--- MatchingGame.java
+++ MatchingGame.java
@@ -6,9 +6,9 @@ public class MatchingGame {
     JFrame frame = new JFrame("Matching Game");
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     frame.setSize(800,600);
-
+    frame.setBackground(new Color(230, 162, 46));
+    frame.setForeground(new Color(230, 162, 46));
     frame.add(new GamePanel());
-
     frame.setLayout(new FlowLayout());
     frame.setVisible(true);
   }
